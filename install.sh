d6966ab1a1b0833a74407c61ab20bdf65b94efc7
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-09-07 14:09:01 +0200

    chore: Update Docker installation script and remove unnecessary commands

--- install.sh
+++ install.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-#Update
-sudo apt update
-
-#Install Docker
-sudo apt-get -y install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-#Add user to Docker group
-sudo usermod -aG docker $USER
-newgrp docker
-
-echo "Docker has been successfully installed."
-
 #creating folders for Docker
 mkdir "grafana"
 chmod 777 "grafana"
